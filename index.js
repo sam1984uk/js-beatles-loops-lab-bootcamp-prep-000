5820e792b3be2e18ec39ae113ebe8e521143f152
sam ditson <samditson2@hotmail.co.uk>
2018-08-04 18:02:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function johnLennonFacts(facts){
 
 function iLoveTheBeatles(n){
   var empty = new Array();
-  do {return empty.push("I love the Beatles!");
+  do {empty.push("I love the Beatles!");
   n++}
   while (n < 15);
   return empty
